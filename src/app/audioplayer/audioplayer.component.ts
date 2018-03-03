96d2338f3e697b5f7fe55475beb73ad34e084e32
__\_ <billguastalla@gmail.com>
2018-03-03 18:47:28 +0000

    -> html5 audio object added to component and initialised/destroyed on init/destroy.

--- src/app/audioplayer/audioplayer.component.ts
+++ src/app/audioplayer/audioplayer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-audioplayer',
   templateUrl: './audioplayer.component.html',
   styleUrls: ['./audioplayer.component.css']
 })
-export class AudioplayerComponent implements OnInit {
+export class AudioplayerComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  private __audio: any = null;
 
+  ngOnInit() {
+    this.__audio = new Audio();
+  }
+  ngOnDestroy() {
+    if (this.__audio) {
+      this.__audio.pause();
+      this.__audio = null;
+    }
+  }
 }
