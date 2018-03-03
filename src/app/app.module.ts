55d9ec872761864f00102db4cb52fd8fbafbeec4
__\_ <billguastalla@gmail.com>
2018-03-03 18:20:34 +0000

    -> Created audioplayer as an empty component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AudioplayerComponent } from './audioplayer/audioplayer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioplayerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/audioplayer/audioplayer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audioplayer',
+  templateUrl: './audioplayer.component.html',
+  styleUrls: ['./audioplayer.component.css']
+})
+export class AudioplayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
